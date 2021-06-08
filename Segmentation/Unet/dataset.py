5ff24981f1657a22e8711d6774a175f4dd29dbf0
yuemengrui <yuemengrui@aistreamrpa.com>
2021-06-08 09:51:20 +0800

    UNet_table_seg modify dataset

--- Segmentation/Unet/dataset.py
+++ Segmentation/Unet/dataset.py
@@ -22,8 +22,9 @@ class TableSegDataset(Dataset):
             self.img_32n = False
         else:
             self.img_32n = True
+
         self.transform = transforms.Compose([transforms.ToTensor(),
-                                             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                                             transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
                                              ])
         if self.mode == 'train':
             train_label_path = os.path.join(data_dir, 'train.txt')
